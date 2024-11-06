df889b541eb15e3558ac2b463c176e7ef94c997a
Jakob <jakob5822@gmail.com>
2024-11-06 09:45:34 +0100

    Computer works kinda

---
+++ NPC Unity folder/Assets/Import/PC/MouseScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseScript : MonoBehaviour
+{
+    public Transform mouseCenter;
+    public float maxXDistance = 0.5f;
+    public float maxZDistance = 0.5f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("hand"))
+        {
+            Vector3 newPosition = new Vector3(other.transform.position.x, transform.position.y, other.transform.position.z);
+
+            newPosition.x = Mathf.Clamp(newPosition.x, mouseCenter.position.x - maxXDistance, mouseCenter.position.x + maxXDistance);
+            newPosition.z = Mathf.Clamp(newPosition.z, mouseCenter.position.z - maxZDistance, mouseCenter.position.z + maxZDistance);
+
+            transform.position = newPosition;
+        }
+    }
+}
---
+++ NPC Unity folder/Assets/Import/PC/PCMouseScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PCMouseScript : MonoBehaviour
+{
+    public GameObject mouseObject;
+    public float sensitivity;
+
+    public float maxY, maxX;
+    
+    void Update()
+    {
+        Vector3 poo = new Vector3(mouseObject.transform.localPosition.x * sensitivity, mouseObject.transform.localPosition.z * sensitivity, transform.localPosition.z);
+
+        poo.x = Mathf.Clamp(transform.localPosition.x, transform.localPosition.x - maxX, transform.localPosition.x + maxX);
+        poo.y = Mathf.Clamp(transform.localPosition.y, transform.localPosition.y - maxY, transform.localPosition.y + maxY);
+
+        transform.localPosition = new Vector3(mouseObject.transform.localPosition.x * sensitivity, mouseObject.transform.localPosition.z * sensitivity, transform.localPosition.z)
+    }
+
+    //InputDevice
+}
