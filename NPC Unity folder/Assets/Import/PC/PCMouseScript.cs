526ee61acb766141a2dfb594e0ac3a784ab2ce8e
Jakob <jakob5822@gmail.com>
2024-11-06 11:47:04 +0100

    Jeg har ændret tags

--- NPC Unity folder/Assets/Import/PC/PCMouseScript.cs
+++ NPC Unity folder/Assets/Import/PC/PCMouseScript.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.DeviceSimulation;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 
 public class PCMouseScript : MonoBehaviour
 {
@@ -9,21 +12,48 @@ public class PCMouseScript : MonoBehaviour
     public float sensitivity;
 
     public float maxY, maxX;
-    
+
+    [SerializeField]GameObject vrControllerHandRight;
+    ControllerAnimator yoinkScript;
+
+    private void Start()
+    {
+        yoinkScript = vrControllerHandRight.GetComponent<ControllerAnimator>();
+    }
+
     void Update()
     {
-        Vector3 poo = new Vector3(mouseObject.transform.localPosition.x * sensitivity, mouseObject.transform.localPosition.z * sensitivity, transform.localPosition.z);
+        Vector3 followPosition = new Vector3(mouseObject.transform.localPosition.x * sensitivity, mouseObject.transform.localPosition.z * sensitivity, transform.localPosition.z);
 
-        poo.x = Mathf.Clamp(transform.localPosition.x, transform.localPosition.x - maxX, transform.localPosition.x + maxX);
-        poo.y = Mathf.Clamp(transform.localPosition.y, transform.localPosition.y - maxY, transform.localPosition.y + maxY);
+        followPosition.x = Mathf.Clamp(transform.localPosition.x, transform.localPosition.x - maxX, transform.localPosition.x + maxX);
+        followPosition.y = Mathf.Clamp(transform.localPosition.y, transform.localPosition.y - maxY, transform.localPosition.y + maxY);
 
         transform.localPosition = new Vector3(mouseObject.transform.localPosition.x * sensitivity, mouseObject.transform.localPosition.z * sensitivity, transform.localPosition.z);
+
+        //Debug.Log(yoinkScript.m_TriggerInput);
     }
 
     //InputDevice
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.CompareTag("popup"))
+        {
+
+            Debug.Log("Entering!");
+        }
+
+        if (other.gameObject.CompareTag("popupVirus"))
+        {
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("popup"))
+        {
+            Debug.Log("Exiting!");
+        }
     }
 }
