ea6bbde3b3f3d0c11a0ebd407b3b6fa409bff29c
Goodest person <mflens22@student.aau.dk>
2024-11-07 14:45:12 +0100

    Working printer
    Colliders need work

--- NPC Unity folder/Assets/WorkingPrinter/Printerscript.cs
+++ NPC Unity folder/Assets/WorkingPrinter/Printerscript.cs
@@ -2,15 +2,24 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
 
 public class Printerscript : MonoBehaviour
 {
 
     public GameObject Paper;
-
-    public Transform PaperPosition;
+    
+    public Transform[] SpawnPositions;
+    public Transform Startpos1;
+    public Transform Endpos1;
+    
     
     private GameObject currentPaper;
+    
+    public bool isMoving = false;
+    public float moveSpeed = 1.0f;
+    public float moveProgress = 0.0f;
     
     // Start is called before the first frame update
     void Start()
@@ -21,21 +30,36 @@ public class Printerscript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isMoving)
+        {
+            moveProgress += Time.deltaTime * moveSpeed;
+
+            currentPaper.transform.position = Vector3.Lerp(Startpos1.position, Endpos1.position, moveProgress );
+            
+            if (moveProgress >= 1.0f)
+            {
+                isMoving = false;
+                moveProgress = 0.0f;
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Triggered");
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && !isMoving)
         {
+            currentPaper = Instantiate(Paper, Startpos1.position, Quaternion.identity);
             
-            currentPaper = Instantiate(Paper, PaperPosition.position, PaperPosition.rotation);
+            // int randomIndex = Random.Range(0, SpawnPositions.Length);
+            // Vector3 spawnPos = SpawnPositions[randomIndex].position;
+            // currentPaper = Instantiate(Paper, spawnPos, Quaternion.identity);
             //Move paper to set location
             print("Paper printed :3");
 
+            isMoving = true;
+            moveProgress = 0.0f;
             
-
         }
     }
 }
